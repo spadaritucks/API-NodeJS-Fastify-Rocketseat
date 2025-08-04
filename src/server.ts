de26b0ef6a5655a715e34a31605512e3bff35012
spadaritucks <thiago.spadari02@gmail.com>
2025-08-04 13:53:54 -0300

    fix: add missing host configuration in server listen options

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,8 @@ import { app } from "./app.js"
 import { env } from "./env/index.js"
 
 app.listen({
-    port : env.PORT
+    port: env.PORT,
+    host: '0.0.0.0',
 }).then(() => {
     console.log("Http server running on port", env.PORT)
 })
